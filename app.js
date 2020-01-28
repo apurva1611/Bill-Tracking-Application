8d992a389ef39b364fcbb0da6ef490b87aee582e
apurva1611 <mathurapurva16@gmail.com>
2020-01-28 11:13:54 -0500

    assignment3

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const billController = require('./controllers/billController');
+const billsRouter = require('./routes/billRoutes');
 const userRouter = require('./routes/userRoutes');
 const app = express();
 app.use(bodyparser.json());
 
 // 3) ROUTES
 app.use('/v1/user/', userRouter);
+app.use('/v1/bill/', billsRouter);
+app.get('/v1/bills/',billController.checkUser,billController.getAllBills);
 module.exports = app;
