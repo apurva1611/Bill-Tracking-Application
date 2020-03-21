07a5983b389dd70dc71f25742a3fcac6c0791da6
apurva1611 <mathurapurva16@gmail.com>
2020-03-21 18:30:51 -0400

    cloudwatch added

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,10 +5,8 @@ ls -al
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -a fetch-config \
     -m ec2 \
-    -c file: /home/ubuntu/cloudwatch-config.json \
+    -c file: /cloudwatch-config.json \
     -s
-sudo systemctl start amazon-cloudwatch-agent.service
-sudo systemctl enable amazon-cloudwatch-agent.service
 cd /home/ubuntu/webapp_node
 npm install
 rm -rf node_modules/bcrypt
