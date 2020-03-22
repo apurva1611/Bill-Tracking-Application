6fe9c242c158392ecc422a19dad2d150c078aa93
apurva1611 <mathurapurva16@gmail.com>
2020-03-22 17:05:34 -0400

    logs

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,2 @@
 cd /opt/codedeploy-agent/deployment-root/deployment-instructions/
 sudo rm -rf *-cleanup
-sudo rm -rf /home/ubuntu/config.json
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,3 +5,8 @@
 ls -al
 sudo pm2 stop /home/ubuntu/webapp_node/server.js
 sudo pm2 start /home/ubuntu/webapp_node/server.js
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file: /home/ubuntu/webapp_node/cloudwatch-config.json \
+    -s
