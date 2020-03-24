6ef5e16b14ce05a570a026ab21ed4f3483f151b5
apurva1611 <mathurapurva16@gmail.com>
2020-03-24 19:02:05 -0400

    ls removed

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,7 +2,6 @@
 # sudo chmod 755 /var/www/server.js # optional
 # this will restart app/server on instance reboot
 # actually start the server
-ls -al
 sudo pm2 stop /home/ubuntu/webapp_node/server.js
 sudo pm2 start /home/ubuntu/webapp_node/server.js
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/webapp_node/cloudwatch-config.json -s
